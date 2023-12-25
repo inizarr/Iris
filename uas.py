5d38097027eccb806ebb3641139ac9df2ff5ef30
inizarr <149244950+inizarr@users.noreply.github.com>
2023-12-25 17:38:36 +0700

    Update uas.py

--- uas.py
+++ uas.py
@@ -3,6 +3,32 @@ import numpy as np
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 import streamlit as st
+from web_functions import load_data
+
+from Tabs import home, predict, visualise
+# import library yang dibutuhkan
+
+Tabs = {
+    "Home" : home,
+    "Prediction" : predict,
+    "Visualisation" : visualise    
+}
+
+# membuat sidebar
+st.sidebar.title("Navigasi")
+
+# membuat radio option
+page = st.sidebar.radio("Pages", list(Tabs.keys()))
+
+# load dataset
+df, x, y = load_data()
+
+# kondisi call app function
+if page in ["Prediction", "Visualisation"]:
+    Tabs[page].app(df,x,y)
+else:
+    Tabs[page].app()
+
 
 st.cache_data ()
 def load_data():
