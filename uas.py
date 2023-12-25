7fab795ca5f6a6285ad3f688edb6e7c14e54fe1b
inizarr <149244950+inizarr@users.noreply.github.com>
2023-12-25 16:03:12 +0700

    Update uas.py

--- uas.py
+++ uas.py
@@ -4,9 +4,6 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
-
-
-
 # Upload the dataset
 st.title("D-Tree Web Application")
 uploaded_file = st.file_uploader("Iris.csv")
