789968319866eecd98d1301a70ce23fff100aba9
inizarr <149244950+inizarr@users.noreply.github.com>
2023-12-25 16:02:11 +0700

    Update uas.py

--- uas.py
+++ uas.py
@@ -4,7 +4,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
-import plotly.express as px
+
 
 
 # Upload the dataset
