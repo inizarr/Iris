aa8eea34168d987aef90f0e121f5f843f7c78440
inizarr <149244950+inizarr@users.noreply.github.com>
2023-12-25 16:36:51 +0700

    Update uas.py

--- uas.py
+++ uas.py
@@ -1,13 +1,12 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 
 # Upload the dataset
-st.title("D-Tree Web Application")
-uploaded_file = st.file_uploader("Choose a file")
+st.title("Aplikasi Web Pohon Keputusan")
+uploaded_file = st.file_uploader("Pilih file")
 
 # Check if a file is uploaded
 if uploaded_file is not None:
@@ -17,19 +16,19 @@ if uploaded_file is not None:
     # Check if the dataset has at least 2 columns
     if len(df.columns) >= 2:
         # Show the first 10 rows of the dataset
-        st.write("First 10 rows of the dataset:")
+        st.write("10 baris pertama dataset:")
         st.dataframe(df.head(10))
 
         # Choose the target column
-        target_col = st.selectbox("Select the target column", df.columns)
+        target_col = st.selectbox("Pilih kolom target", df.columns)
 
         # Split the dataset into features and target
         features = df.drop(columns=[target_col])
-       target_train = df[target_col]
+        target_train = df[target_col]
 
         # Split the dataset into training and testing sets
         features_train, features_test, target_train, target_test = train_test_split(
-            features, target, test_size=0.2, random_state=42
+            features, target_train, test_size=0.2, random_state=42
         )
 
         # Create the Decision Tree classifier
@@ -45,20 +44,4 @@ if uploaded_file is not None:
         accuracy = accuracy_score(target_test, target_pred)
 
         # Show the accuracy of the model
-        st.write(f"Accuracy of the model: {accuracy:.2f}")
-
-        # Create the plotly express tree plot
-        tree_plot = px.tree(
-            clf,
-            path=['True', 'False', 'color = LightSeaGreen'],
-            values=features_train.iloc[:5, 1].values,
-            hover_data=df.iloc[:5, 2:],
-            title='Decision Tree Plot'
-        )
-
-        # Display the plotly express tree plot
-        st.plotly_chart(tree_plot)
-    else:
-        st.write("Please upload a dataset with at least 2 columns.")
-else:
-    st.write("Iris.csv")
+        st.write(f"Akurasi model: {accuracy:.2f}")
