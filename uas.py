a73d178b5bbc2944b8fad4d32acb29d6ee20cf37
inizarr <149244950+inizarr@users.noreply.github.com>
2023-12-30 15:50:46 +0700

    Update uas.py

--- uas.py
+++ uas.py
@@ -12,8 +12,10 @@ y = iris.target
 # Membagi dataset menjadi data latih dan data uji
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Membuat model decision tree
+# Membuat model Decision Tree
 model = DecisionTreeClassifier()
+
+# Melatih model dengan data latih
 model.fit(X_train, y_train)
 
 # Memprediksi kelas untuk data uji
